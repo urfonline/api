8190ee3735a48bb667d5c1d5aed9c52e1f09c010
James Canning <james@brudil.com>
2017-11-19 17:20:16 +0000

    Add overrides to homepage blocks

--- api/home/models.py
+++ api/home/models.py
@@ -6,6 +6,9 @@ POSITIONS = (
     ('HERO', 'Hero'),
     ('SEC_1', 'Secondary 1'),
     ('SEC_2', 'Secondary 2'),
+    ('THIRD_1', 'Third 1'),
+    ('THIRD_2', 'Third 2'),
+    ('THIRD_3', 'Third 3'),
 )
 
 
@@ -21,3 +24,8 @@ class HomepageBlock(models.Model):
     published_at = models.DateTimeField()
 
     position = models.CharField(max_length=12, choices=POSITIONS)
+
+    override_kicker = models.CharField(max_length=64, default='')
+    override_title = models.CharField(max_length=265, default='')
+    override_description = models.TextField(default='')
+    override_background_color = models.CharField(max_length=64, default='')
