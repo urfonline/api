e644d757d260ce407e5548c69ead6d01f7f896b9
Henry Le Grys <henry@legrys.info>
2019-10-04 16:22:34 +0100

    Add endpoint for fetching Icecast status of a stream

--- api/streams/views.py
+++ api/streams/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from api.streams.models import StreamConfiguration
+from django.http import HttpResponse
+from django.http.request import HttpRequest
+import requests
 
-# Create your views here.
+def get_stream_status(request: HttpRequest, stream_slug: str):
+    stream = StreamConfiguration.objects.get(slug=stream_slug)
+
+    r = requests.get(f'http://{stream.host}:{stream.port}/status-json.xsl')
+    r.raise_for_status()
+
+    return HttpResponse(r.json())
