1bbe3d983f9828c5a12872c10297028377370743
Henry <omgredstone@gmail.com>
2019-10-05 02:35:33 +0100

    Fix URL pattern regex & return response as JSON
    Also fixed use of f-string syntax; we're on python 3.5 so the .format syntax is required

--- api/streams/urls.py
+++ api/streams/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^([-a-zA-Z0-9_])/status$', views.get_stream_status)
+    url(r'^([-a-zA-Z0-9_]+)/status$', views.get_stream_status)
 ]
--- api/streams/views.py
+++ api/streams/views.py
@@ -1,12 +1,12 @@
 from api.streams.models import StreamConfiguration
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.http.request import HttpRequest
 import requests
 
 def get_stream_status(request: HttpRequest, stream_slug: str):
     stream = StreamConfiguration.objects.get(slug=stream_slug)
 
-    r = requests.get(f'http://{stream.host}:{stream.port}/status-json.xsl')
+    r = requests.get('http://{stream.host}:{stream.port}/status-json.xsl'.format(stream=stream))
     r.raise_for_status()
 
-    return HttpResponse(r.json())
+    return JsonResponse(r.json())
