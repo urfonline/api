151b78036067e2b8cec7bd0eabaad5d27b2c37c1
Henry <omgredstone@gmail.com>
2019-10-07 18:49:35 +0100

    Handle upstream timeouts gracefully

--- api/streams/views.py
+++ api/streams/views.py
@@ -6,12 +6,14 @@ import requests
 def get_stream_status(request: HttpRequest, stream_slug: str):
     try:
         stream = StreamConfiguration.objects.get(slug=stream_slug)
+
+        r = requests.get('http://{stream.host}:{stream.port}/status-json.xsl'.format(stream=stream), timeout=5)
     except StreamConfiguration.DoesNotExist:
         raise Http404("Stream with slug {0} does not exist.".format(stream_slug))
-
-    r = requests.get('http://{stream.host}:{stream.port}/status-json.xsl'.format(stream=stream), timeout=5)
+    except requests.exceptions.Timeout:
+        return JsonResponse({ "error": "Upstream request timed out" }, status=504)
 
     if r.status_code != requests.codes.ok:
-        return JsonResponse({ "error": "Upstream request failed" }, status=500)
+        return JsonResponse({ "error": "Upstream request failed" }, status=502)
 
     return JsonResponse(r.json())
