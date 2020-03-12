c8436d0aa9ff44e0e2e3a35e91fcf9066dc4e804
Henry <henry@legrys.info>
2020-03-12 04:05:39 +0000

    Update Stream model with proxy URL & mobile mountpoint fields

--- api/streams/models.py
+++ api/streams/models.py
@@ -8,6 +8,8 @@ class StreamConfiguration(models.Model):
     host = models.CharField(max_length=80, null=False)
     port = models.IntegerField(blank=False, null=False)
     mountpoint = models.CharField(max_length=40, default="/stream", null=False)
+    mobile_mountpoint = models.CharField(max_length=40, default="/mobile", null=False)
+    proxy_url = models.CharField(max_length=70, default=None, null=True, blank=True)
     priority_online = models.IntegerField(blank=False, null=False, default=10, verbose_name="Online Priority")
     priority_offline = models.IntegerField(blank=False, null=False, default=3, verbose_name="Bed Priority")
     slate = models.ForeignKey(ScheduleSlate, null=True, blank=True, verbose_name="Active Slate")
