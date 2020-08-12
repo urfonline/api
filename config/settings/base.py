af99844466ebd19ebadcfc5786abe0f41ba74f3a
Henry Le Grys <henry@legrys.info>
2020-08-12 18:33:27 +0100

    Remove awesome-slugify & upgrade django again

--- config/settings/base.py
+++ config/settings/base.py
@@ -293,9 +293,6 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'users:redirect'
 LOGIN_URL = 'account_login'
 
-# SLUGLIFIER
-AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
-
 # Location of root django.contrib.admin URL, use {% url 'admin:index' %}
 ADMIN_URL = 'admin/'
 
