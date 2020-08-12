4e395215e900abb901eeb3e5014e12bc0e4687f6
Henry Le Grys <henry@legrys.info>
2020-08-12 14:10:36 +0100

    Update main URL patterns to use new path function

--- config/settings/base.py
+++ config/settings/base.py
@@ -307,7 +307,7 @@ else:
 
 
 # Location of root django.contrib.admin URL, use {% url 'admin:index' %}
-ADMIN_URL = r'^admin/'
+ADMIN_URL = 'admin/'
 
 # Your common stuff: Below this line define 3rd party library settings
 
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import path, include
 from django.views import defaults as default_views
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
@@ -34,35 +34,35 @@ class GraphQLWithAuthView(GraphQLView):
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    path('', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
-    url(settings.ADMIN_URL, admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
 
     # User management
-    url(r'^users/', include('api.users.urls')),
-    url(r'^streams/', include('api.streams.urls')),
-    url(r'^applications/', include('api.applications.urls')),
-    url(r'^accounts/', include('allauth.urls')),
-    url(r'^graphql', csrf_exempt(GraphQLWithAuthView.as_view(graphiql=True))),
-    url(r'^cms/', include(wagtailadmin_urls)),
-    url(r'^documents/', include(wagtaildocs_urls)),
-    url(r'^pages/', include(wagtail_urls)),
-    url(r'^cms-api/v2/', api_router.urls),
+    path('users/', include('api.users.urls')),
+    path('streams/', include('api.streams.urls')),
+    path('applications/', include('api.applications.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('graphql', csrf_exempt(GraphQLWithAuthView.as_view(graphiql=True))),
+    path('cms/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('pages/', include(wagtail_urls)),
+    path('cms-api/v2/', api_router.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
-        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
-        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
-        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
-        url(r'^500/$', default_views.server_error),
+        path('400/', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
+        path('403/', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
+        path('404/', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
+        path('500/', default_views.server_error),
     ]
     if 'debug_toolbar' in settings.INSTALLED_APPS:
         import debug_toolbar
         urlpatterns = [
-            url(r'^__debug__/', include(debug_toolbar.urls)),
+            path('__debug__/', include(debug_toolbar.urls)),
         ] + urlpatterns
