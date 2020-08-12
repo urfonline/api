8531377a41d8bb398cf07481944d4a538ae4ace1
Henry Le Grys <henry@legrys.info>
2020-08-12 18:50:06 +0100

    Remove deprecated wagtail SiteMiddleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -101,7 +101,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
