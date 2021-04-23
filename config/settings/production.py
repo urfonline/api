99316ba0112a5ea81fae83019d3bf07e4de57241
Henry Le Grys <henry@legrys.info>
2021-04-23 05:08:48 +0100

    Fix cors origin whitelist

--- config/settings/production.py
+++ config/settings/production.py
@@ -65,7 +65,11 @@ ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['api.urfonline.com', ]
 
 # CORS & CSRF configuration
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = ["www.urfonline.com", "76podcasting.net", "beta.76podcasting.net"]
+CORS_ORIGIN_WHITELIST = [
+    "https://www.urfonline.com",
+    "https://76podcasting.net",
+    "https://beta.76podcasting.net",
+]
 CSRF_TRUSTED_ORIGINS = [".urfonline.com"]
 CORS_ALLOW_CREDENTIALS = False
 
