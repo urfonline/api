f707dc73b177a7a36c94d9f8dab808cdf146e24e
Henry Le Grys <henry@legrys.info>
2021-10-27 00:12:50 +0100

    API-D Improve handling of stream status fetching
    Now that we have at least some sharpstream API use, tidy up the fetching
    function and better handle error cases

--- api/streams/views.py
+++ api/streams/views.py
@@ -21,7 +21,7 @@ def process_response(stream: StreamConfiguration, data):
 
         return {
             "offline": not is_online,
-            "description": "SharpStream",
+            "description": "Live",
         }
     else:
         return {
@@ -34,7 +34,7 @@ def get_stream_status(request: HttpRequest, stream_slug: str):
         stream = StreamConfiguration.objects.get(slug=stream_slug)
 
         if stream.status_url == "" or stream.status_url is None:
-            return JsonResponse(process_response(stream, {}))
+            return JsonResponse({"error": "No status URL available"}, status=500)
 
         headers = {}
         if stream.type == "SHARPSTREAM":
@@ -42,14 +42,16 @@ def get_stream_status(request: HttpRequest, stream_slug: str):
             headers["Accept"] = "application/json"
 
         r = requests.get(stream.status_url, timeout=7, headers=headers)
+
+        if r.status_code != requests.codes.ok:
+            return JsonResponse({"error": "Upstream request failed"}, status=502)
+
+        return JsonResponse(process_response(stream, r.json()))
     except StreamConfiguration.DoesNotExist:
         raise Http404("Stream with slug {0} does not exist.".format(stream_slug))
     except requests.exceptions.Timeout:
         return JsonResponse({"error": "Upstream request timed out"}, status=504)
     except requests.exceptions.RequestException:
-        return JsonResponse({"error": "Upstream request failed"}, status=504)
-
-    if r.status_code != requests.codes.ok:
         return JsonResponse({"error": "Upstream request failed"}, status=502)
-
-    return JsonResponse(process_response(stream, r.json()))
+    except (KeyError, IndexError, ValueError):
+        return JsonResponse({"error": "Failed decoding upstream response"}, status=500)
